0a7fcf399fb854a29c405d2d79f579d5a910bcc1
Elian Doran <contact@eliandoran.me>
2024-07-13 11:06:45 +0300

    docker: Fix webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,10 @@
 const path = require('path');
-const assetPath = require('./src/services/asset_path');
+import assetPath = require('./src/services/asset_path');
 
 module.exports = {
     mode: 'production',
     entry: {
-        setup: './src/public/app/setup.ts',
+        setup: './src/public/app/setup.js',
         mobile: './src/public/app/mobile.js',
         desktop: './src/public/app/desktop.js',
     },
