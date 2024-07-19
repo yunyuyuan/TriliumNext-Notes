ab234592990c2e47edb886eecfe494a368a5a249
Elian Doran <contact@eliandoran.me>
2024-07-19 19:30:27 +0300

    server: Fix script execution error (closes #244)

--- src/services/backend_script_api_interface.ts
+++ src/services/backend_script_api_interface.ts
@@ -3,8 +3,8 @@ import AbstractBeccaEntity = require("../becca/entities/abstract_becca_entity");
 import BNote = require("../becca/entities/bnote");
 
 export interface ApiParams {
-    startNote?: BNote;
-    originEntity?: AbstractBeccaEntity<any>;
+    startNote?: BNote | null;
+    originEntity?: AbstractBeccaEntity<any> | null;
     pathParams?: string[],
     req?: Request,
     res?: Response
