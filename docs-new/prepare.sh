5bcae52d081ab96c75f54c104c35bb4fbf6af8b1
Elian Doran <contact@eliandoran.me>
2024-07-20 13:08:22 +0300

    docs: Always use HTML extensions

--- docs-new/prepare.sh
+++ docs-new/prepare.sh
@@ -15,16 +15,11 @@ source ./.env
 
 # Download everything in output/notes.example.com/share/...
 share_url="$SHARE_PROTOCOL://$SHARE_HOST/share/$ROOT_NOTE_ID"
-wget -rp -e robots=off "$share_url" -P "$output_dir"
+wget -rpEk -e robots=off "$share_url" -P "$output_dir"
 
 # Get rid of the domain in the output folder
 mv "$output_dir/$SHARE_HOST"/* "$output_dir/"
 rmdir "$output_dir/$SHARE_HOST"
-
-# Rename share/* to share/*.html because GitHub Pages will ask the client to download the files otherwise.
-for file in "$output_dir/share"/*; do
-    mv "$file" "$file.html"
-done
 
 # Create home page with redirect
 index_dest_path="$output_dir/index.html"
