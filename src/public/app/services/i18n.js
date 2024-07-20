0dbe6702ff1faf750ac7982a5b23c52f0db40408
Elian Doran <contact@eliandoran.me>
2024-07-20 09:42:55 +0300

    i18n: Translate about page

--- src/public/app/services/i18n.js
+++ src/public/app/services/i18n.js
@@ -2,4 +2,25 @@ import library_loader from "./library_loader.js";
 
 await library_loader.requireLibrary(library_loader.I18NEXT);
 
+i18next.init({
+    lng: "en",
+    debug: true,
+    resources: {
+        en: {
+            translation: {
+                about: {
+                    title: "About TriliumNext Notes",
+                    homepage: "Homepage:",
+                    app_version: "App version:",
+                    db_version: "DB version:",
+                    sync_version: "Sync version:",
+                    build_date: "Build date:",
+                    build_revision: "Build revision:",
+                    data_directory: "Data directory:"
+                }
+            }
+        }
+    }
+});
+
 export const t = i18next.t;
