e284ea945c0d2d3a7e1cae7566bae9d2f80f15eb
Elian Doran <contact@eliandoran.me>
2024-07-20 11:42:54 +0300

    i18n: Improve formatting

--- src/public/app/services/library_loader.js
+++ src/public/app/services/library_loader.js
@@ -72,10 +72,12 @@ const MARKJS = {
     ]
 };
 
-const I18NEXT = { js: [
-    "node_modules/i18next/i18next.min.js",
-    "node_modules/i18next-http-backend/i18nextHttpBackend.min.js"
-] };
+const I18NEXT = {
+    js: [
+        "node_modules/i18next/i18next.min.js",
+        "node_modules/i18next-http-backend/i18nextHttpBackend.min.js"
+    ]
+};
 
 async function requireLibrary(library) {
     if (library.css) {
