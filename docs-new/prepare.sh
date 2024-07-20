621cc4a0651f0dc0ac3054f7117133d6c47b7098
Elian Doran <contact@eliandoran.me>
2024-07-20 12:26:52 +0300

    docs: Get rid of domain in output folder

--- docs-new/prepare.sh
+++ docs-new/prepare.sh
@@ -8,4 +8,11 @@ rm -rf "$output_dir"
 mkdir -p "$output_dir"
 
 source ./.env
-wget -rp -e robots=off "$SHARE_URL" -P "$output_dir"
+
+# Download everything in output/notes.example.com/share/...
+share_url="$SHARE_PROTOCOL://$SHARE_HOST/share/$ROOT_NOTE_ID"
+wget -rp -e robots=off "$share_url" -P "$output_dir"
+
+# Get rid of the domain in the output folder
+mv "$output_dir/$SHARE_HOST"/* "$output_dir/"
+rmdir "$output_dir/$SHARE_HOST"
