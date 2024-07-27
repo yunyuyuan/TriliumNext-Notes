32dc064ffab38056bf0563b42143c5015fc2f232
Elian Doran <contact@eliandoran.me>
2024-07-27 23:49:56 +0300

    docs: Add some more documentation

--- bin/docs/prepare.sh
+++ bin/docs/prepare.sh
@@ -21,6 +21,10 @@ source "$env_file"
 # Download everything in output/notes.example.com/share/...
 share_url="$SHARE_PROTOCOL://$SHARE_HOST/share/$ROOT_NOTE_ID"
 wget -rpEk -e robots=off "$share_url" -P "$output_dir"
+if [ $? -ne 0 ]; then
+    echo -e \\nDownloading failed, make sure you are using the real wget package and not the busybox one.
+    exit 1
+fi
 
 # Get rid of the domain in the output folder
 mv "$output_dir/$SHARE_HOST"/* "$output_dir/"
