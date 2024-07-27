f5e02102e7abafa61034a7b6fd7e773c29871a86
Elian Doran <contact@eliandoran.me>
2024-07-27 23:27:18 +0300

    docs: Improve the preview script

--- bin/docs/preview.sh
+++ bin/docs/preview.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+LISTEN_ADDRESS=127.0.0.1:8088
+
 script_dir=$(realpath $(dirname $0))
-output_dir="$script_dir/../docs"
-httpd -fv -p 127.0.0.1:8089 -h "$output_dir"
+output_dir="$script_dir/../../docs"
+echo "Preview the documentation at http://$LISTEN_ADDRESS"
+httpd -fv -p "$LISTEN_ADDRESS" -h "$output_dir"
