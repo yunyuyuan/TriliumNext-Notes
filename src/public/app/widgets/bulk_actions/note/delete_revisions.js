50434201cfd2a3f1364a5434feea8efee0b64808
Nriver <6752679+Nriver@users.noreply.github.com>
2024-07-30 09:40:02 +0800

    add translation for bulk action of notes

--- src/public/app/widgets/bulk_actions/note/delete_revisions.js
+++ src/public/app/widgets/bulk_actions/note/delete_revisions.js
@@ -1,27 +1,26 @@
+import { t } from "../../../services/i18n.js";
 import AbstractBulkAction from "../abstract_bulk_action.js";
 
 const TPL = `
 <tr>
     <td colspan="2">
         <span class="bx bx-trash"></span>
-    
-        Delete note revisions
+        ${t('delete_revisions.delete_note_revisions')}
     </td>
     <td class="button-column">
         <div class="dropdown help-dropdown">
             <span class="bx bx-help-circle icon-action" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></span>
             <div class="dropdown-menu dropdown-menu-right p-4">
-                All past note revisions of matched notes will be deleted. Note itself will be fully preserved. In other terms, note's history will be removed.
+                ${t('delete_revisions.all_past_note_revisions')}
             </div>
         </div>
-    
         <span class="bx bx-x icon-action action-conf-del"></span>
     </td>
 </tr>`;
 
 export default class DeleteRevisionsBulkAction extends AbstractBulkAction {
     static get actionName() { return "deleteRevisions"; }
-    static get actionTitle() { return "Delete note revisions"; }
+    static get actionTitle() { return t('delete_revisions.delete_note_revisions'); }
 
     doRender() {
         return $(TPL);
--- src/public/app/widgets/bulk_actions/note/rename_note.js
+++ src/public/app/widgets/bulk_actions/note/rename_note.js
@@ -1,31 +1,32 @@
 import SpacedUpdate from "../../../services/spaced_update.js";
 import AbstractBulkAction from "../abstract_bulk_action.js";
+import { t } from "../../../services/i18n.js";
 
 const TPL = `
 <tr>
     <td colspan="2">
         <div style="display: flex; align-items: center">
-            <div style="margin-right: 10px; flex-shrink: 0;">Rename note title to:</div> 
+            <div style="margin-right: 10px; flex-shrink: 0;">${t('rename_note.rename_note_title_to')}</div> 
             
             <input type="text" 
                 class="form-control new-title" 
-                placeholder="new note title" 
-                title="Click help icon on the right to see all the options"/>
+                placeholder="${t('rename_note.new_note_title')}" 
+                title="${t('rename_note.click_help_icon')}"/>
         </div>
     </td>
     <td class="button-column">
         <div class="dropdown help-dropdown">
             <span class="bx bx-help-circle icon-action" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></span>
             <div class="dropdown-menu dropdown-menu-right p-4">
-                <p>The given value is evaluated as JavaScript string and thus can be enriched with dynamic content via the injected <code>note</code> variable (note being renamed). Examples:</p>
+                <p>${t('rename_note.evaluated_as_js_string')}</p>
                 
                 <ul>
-                    <li><code>Note</code> - all matched notes are renamed to "Note"</li>
-                    <li><code>NEW: \${note.title}</code> - matched notes titles are prefixed with "NEW: "</li>
-                    <li><code>\${note.dateCreatedObj.format('MM-DD:')}: \${note.title}</code> - matched notes are prefixed with note's creation month-date</li>
+                    <li>${t('rename_note.example_note')}</li>
+                    <li>${t('rename_note.example_new_title')}</li>
+                    <li>${t('rename_note.example_date_prefix')}</li>
                 </ul>
                 
-                See API docs for <a href="https://zadam.github.io/trilium/backend_api/Note.html">note</a> and its <a href="https://day.js.org/docs/en/display/format">dateCreatedObj / utcDateCreatedObj properties</a> for details.
+                ${t('rename_note.api_docs')}
             </div>
         </div>
     
@@ -35,7 +36,7 @@ const TPL = `
 
 export default class RenameNoteBulkAction extends AbstractBulkAction {
     static get actionName() { return "renameNote"; }
-    static get actionTitle() { return "Rename note"; }
+    static get actionTitle() { return t('rename_note.rename_note'); }
 
     doRender() {
         const $action = $(TPL);
