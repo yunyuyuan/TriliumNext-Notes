f81bf509cdf5cd6c3134d530821354212115db60
Elian Doran <contact@eliandoran.me>
2024-08-03 22:20:05 +0300

    build: Create script to build icons for the application

---
+++ bin/create-icons.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+script_dir=$(realpath $(dirname $0))
+cd "${script_dir}/../images/app-icons"
+magick -background none "../icon-color.svg" -resize 180x180 "./ios/apple-touch-icon.png"
+
+# Build PNGs
+magick -background none "../icon-color.svg" -resize "128x128" "./png/128x128.png"
+magick -background none "../icon-color.svg" -resize "256x256" "./png/256x256.png"
+magick -background none "../icon-purple.svg" -resize "256x256" "./png/256x256-dev.png"
+
+# Build Mac .icns
+magick -background none "../icon-color.svg" -resize "512x512" "./png/512x512.png"
+icnsutil compose -f "mac/icon.icns" "./png/512x512.png"
+rm "./png/512x512.png"
+
+# Build Windows icon
+magick -background none "../icon-color.svg" -define icon:auto-resize=16,32,48,64,128,256 "./win/icon.ico";
