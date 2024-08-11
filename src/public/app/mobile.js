22dc4ef997ef97e17fe2a2899d85053573b15ab9
Elian Doran <contact@eliandoran.me>
2024-08-11 08:12:01 +0300

    client: Apply locale selection

--- src/public/app/mobile.js
+++ src/public/app/mobile.js
@@ -1,8 +1,9 @@
 import appContext from "./components/app_context.js";
-import MobileLayout from "./layouts/mobile_layout.js";
 import glob from "./services/glob.js";
 
-glob.setupGlobs();
+glob.setupGlobs()
 
+appContext.earlyInit();
+const MobileLayout = (await import("./layouts/mobile_layout.js")).default;
 appContext.setLayout(new MobileLayout());
 appContext.start();
