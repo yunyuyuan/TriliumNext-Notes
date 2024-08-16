1172908e419bcf052a6fadcb332da720a2cf2b52
Elian Doran <contact@eliandoran.me>
2024-08-16 22:05:53 +0300

    client: Hide tray settings on web

--- integration-tests/settings.spec.ts
+++ integration-tests/settings.spec.ts
@@ -6,3 +6,8 @@ test("Native Title Bar not displayed on web", async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Native Title Bar (requires' })).toBeHidden();
 });
 
+test("Tray settings not displayed on web", async ({ page }) => {
+    await page.goto('http://localhost:8082/#root/_hidden/_options/_optionsOther');
+    await expect(page.getByRole('heading', { name: 'Note Erasure Timeout' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Tray' })).toBeHidden();
+});
--- src/public/app/widgets/type_widgets/options/other/tray.js
+++ src/public/app/widgets/type_widgets/options/other/tray.js
@@ -1,5 +1,6 @@
 import OptionsWidget from "../options_widget.js";
 import { t } from "../../../../services/i18n.js";
+import utils from "../../../../services/utils.js";
 
 const TPL = `
 <div class="options-section">
@@ -17,6 +18,10 @@ export default class TrayOptions extends OptionsWidget {
         this.$trayEnabled = this.$widget.find(".tray-enabled");
         this.$trayEnabled.on('change', () =>
             this.updateOption('disableTray', !this.$trayEnabled.is(":checked") ? "true" : "false"));
+    }
+
+    isEnabled() {
+        return utils.isElectron();
     }
 
     async optionsLoaded(options) {
