1c8cc3640240ac38a9761da48108c6ce0a017fdc
Elian Doran <contact@eliandoran.me>
2024-09-07 10:02:55 +0300

    release 0.90.5-beta

--- src/services/build.ts
+++ src/services/build.ts
@@ -1,4 +1,4 @@
 export default {
-    buildDate: "2024-08-09T22:05:59Z",
-    buildRevision: "2a5c444eff3eb99389339716ea8bfc989be90ecd"
+    buildDate: "2024-09-07T07:02:55Z",
+    buildRevision: "4f6392ce14f2cd727f6ce93499fd0c8ba686321e"
 };
