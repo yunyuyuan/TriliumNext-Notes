317b7b4c595a78e377d6b86b3147c5c893fedc8a
Elian Doran <contact@eliandoran.me>
2024-09-08 18:01:08 +0300

    server: Implement basic restore of language

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -1,10 +1,11 @@
 import i18next from "i18next";
 import Backend from "i18next-fs-backend";
+import options from "./options.js";
 
 export async function initializeTranslations() {
   // Initialize translations
   await i18next.use(Backend).init({
-      lng: "ro",
+      lng: await getCurrentLanguage(),
       fallbackLng: "en",
       ns: "server",
       backend: {
@@ -12,4 +13,16 @@ export async function initializeTranslations() {
       },
       debug: true
   });
+}
+
+function getCurrentLanguage() {
+  let language;
+  language = options.getOption("locale");  
+
+  if (!language) {
+    console.info("Language option not found, falling back to en.");
+    language = "en";
+  }
+
+  return language;
 }
